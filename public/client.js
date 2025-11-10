6db5a7a411fc99db5f8e6b50247e8d1e2e555a08
ctaki2 <161333953+ctaki2@users.noreply.github.com>
2025-11-10 16:42:34 -0600

    Update client.js

--- public/client.js
+++ public/client.js
@@ -8,6 +8,15 @@ window.addEventListener("DOMContentLoaded", () => {
         maxZoom: 19
     }).addTo(map);
 
+    const redIcon = L.icon({
+    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+    
     const audioPlayer = new Audio();
     const songInfo = document.getElementById("songInfo");
     const playBtn = document.getElementById("playBtn");
