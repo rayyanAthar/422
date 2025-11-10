2e378c1c7b15e5f6db64ca2fe83baebac0f5ac15
ctaki2 <161333953+ctaki2@users.noreply.github.com>
2025-11-10 16:27:32 -0600

    Update client.js

--- public/client.js
+++ public/client.js
@@ -2,10 +2,10 @@ const socket = io();
 
 window.addEventListener("DOMContentLoaded", () => {
     const map = L.map("map").setView([41.8781, -87.6298], 12);
-    L.tileLayer("https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png", {
-      maxZoom: 20,
-      attribution:
-        '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a>, &copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 19
     }).addTo(map);
 
     const audioPlayer = new Audio();
