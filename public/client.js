a805abec3145488d5406f37447e4473274ed22bb
ctaki2 <161333953+ctaki2@users.noreply.github.com>
2025-11-10 16:21:29 -0600

    Update client.js

--- public/client.js
+++ public/client.js
@@ -2,8 +2,10 @@ const socket = io();
 
 window.addEventListener("DOMContentLoaded", () => {
     const map = L.map("map").setView([41.8781, -87.6298], 12);
-    L.tileLayer("https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png", {
-        attribution: "&copy; OpenStreetMap contributors"
+    L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 19
     }).addTo(map);
 
     const audioPlayer = new Audio();
