6b31c043c72ec7b85c7f6684393b86274ab49e83
ctaki2 <161333953+ctaki2@users.noreply.github.com>
2025-11-10 16:24:54 -0600

    Update client.js

--- public/client.js
+++ public/client.js
@@ -2,7 +2,7 @@ const socket = io();
 
 window.addEventListener("DOMContentLoaded", () => {
     const map = L.map("map").setView([41.8781, -87.6298], 12);
-    L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
+    L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
         subdomains: 'abcd',
         maxZoom: 19
